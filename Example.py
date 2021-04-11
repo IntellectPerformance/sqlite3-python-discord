adb4397e71d432fd0d574c7b94b9a17db7228594
Zenithican <41877233+SatisfactionEntity@users.noreply.github.com>
2021-04-11 18:03:32 +0200

    Simple Discord.py Code
    An Unique but simple code of discord.py.

---
+++ Example.py
@@ -0,0 +1,37 @@
+# import sqlite3
+# import discord
+
+# SQLite3 Connection Example
+def ConnectSQLite(Type, Command):
+    Database = sqlite3.connect("database name here.db")
+    Cursor = Database.cursor()
+    Cursor.execute(Command)
+    if Type == "Get":
+        AllData = Cursor.fetchall()
+        Database.close()
+        return AllData
+    elif Type == "Set":
+        Database.commit()
+        Database.close()
+        
+# This is the formula i created for calculating the item worth in my discord bot level system.
+
+            #Item_Order1 = (1 * Data[0][7]) digit 7 means the column order where it multiplies the "1" with the amount u currently have.
+            
+# Easy Bot Command Example
+if message.content.startswith('-examplecommand'):
+        
+        Embed = discord.Embed(
+            title = "BotName Commands",
+            description = "Text That u want to be viewed",
+            color = 0x0094ff
+        )
+        await message.channel.send(embed = Embed)
+        
+# Discord Status Example
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Status of your bot'))
+    
+    # Code Made By Sloth And SleepWalking (Milan)
